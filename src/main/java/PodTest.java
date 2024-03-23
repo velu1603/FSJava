0cca12e0d1ba65b1a12f046b2243dadb678861d4
Velan <velu1603@gmail.com>
2024-03-23 17:59:10 +0000

    Added static sum method

--- src/main/java/PodTest.java
+++ src/main/java/PodTest.java
@@ -1,6 +1,14 @@
 public class PodTest {
 
     public static void main(String[] args) {
-        System.out.println("Hi there, I am practicing java again ha ha ha ha");
+        System.out.println("Hi there");
+        sum(10,20);
+    }
+
+    public static void  sum(int a, int b){
+        int c;
+
+        c = a+b;
+        System.out.println(c);
     }
 }
