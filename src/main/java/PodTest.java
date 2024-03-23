b9ed92d2e6d088a8bb5eacbe30295cda0b0331d7
Velan <velu1603@gmail.com>
2024-03-23 18:15:46 +0000

    Added static diff method

--- src/main/java/PodTest.java
+++ src/main/java/PodTest.java
@@ -3,12 +3,28 @@ public class PodTest {
     public static void main(String[] args) {
         System.out.println("Hi there");
         sum(10,20);
+        diff(100,44);
     }
-
+    /*
+    * Sum method
+    *
+    *
+    * */
     public static void  sum(int a, int b){
         int c;
-
         c = a+b;
         System.out.println(c);
     }
+    /*
+     * diff method
+     *
+     *
+     * */
+    public static void  diff(int a, int b){
+        int c;
+        c = a-b;
+        System.out.println(c);
+    }
+
+
 }
